dc32f78cf07d0a3f9dfd51998c602e83888f414e
BishoyElgndin01292902 <n01292902@humber.ca>
2025-07-21 13:06:59 -0400

    Added keyboard event: display last key pressed in real-time

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ hoverBtn.addEventListener('mouseover', () => {
 hoverBtn.addEventListener('mouseout', () => {
   hoverMsg.textContent = 'Mouse left the button!';
 });
+
+// Keyboard Events
+const keyInput = document.getElementById('keyInput');
+const keyMsg = document.getElementById('keyMsg');
+
+keyInput.addEventListener('keyup', (e) => {
+  keyMsg.textContent = `You pressed: ${e.key}`;
+});
